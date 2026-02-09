364d85ad9bd2bbc6b593101fa89a8d0fe5642114
khang <khangau98@example.com>
2026-02-10 00:58:03 +0700

    CI/CD 1

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using FPRDB_SQLite;
+namespace TestProject1
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            // 1. Arrange
+            var calc = new Calculator();
+            int a = 5, b = 3;
+
+            // 2. Act
+            int result = calc.Add(a, b);
+
+            // 3. Assert (Sử dụng FluentAssertions)
+            result.Should().Be(8);
+        }
+    }
+}
