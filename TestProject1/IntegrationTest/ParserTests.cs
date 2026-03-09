511d7a36dc6fff9d49993f8886a6d2a2985aa47c
DevDuyNguyen <kduy91809180@gmail.com>
2026-03-10 02:26:38 +0700

    RecursiveDescentParser::query()

--- TestProject1/IntegrationTest/ParserTests.cs
+++ TestProject1/IntegrationTest/ParserTests.cs
@@ -1233,6 +1233,49 @@ namespace TestProject1.IntegrationTest
             Assert.Equal(true, expected.GetType() == actual.GetType());
             Assert.Equivalent(expected, actual);
         }
+        class Parser_query_testdata : TheoryData<string, QueryData>
+        {
+            public Parser_query_testdata()
+            {
+                //Parser_INTERSECTIONQuery_testdata
+                foreach (var row in new Parser_INTERSECTIONQuery_testdata())
+                    Add((string)row[0], (QueryData)row[1]);
+                //true content:
+                QueryData l1_1 = new BaseCartesianProductQueryData(
+                    new List<SelectField> { new SelectField("", "field1") },
+                    new List<string> { "rel1" },
+                    null
+                    );
+                QueryData l1_2 = new BaseCartesianProductQueryData(
+                    new List<SelectField> { new SelectField("", "field1") },
+                    new List<string> { "rel1" },
+                    null
+                    );
+                var l2_1 = new CompoundQueryData(l1_1, l1_2, SetConnective.UNION, ProbabilisticCombinationStrategy.DISJUNCTION_IGNORANCE);
+                var l2_2 = new BaseCartesianProductQueryData(
+                    new List<SelectField> { new SelectField("", "field1") },
+                    new List<string> { "rel1" },
+                    null
+                    );
+                QueryData root = new CompoundQueryData(l2_2, l2_1, SetConnective.UNION, ProbabilisticCombinationStrategy.DISJUNCTION_IGNORANCE);
+                Add("select field1 from rel1 intersect ⨂_ig (select field1 from rel1 union ⨁_ig select field1 from rel1)", root);
+
+            }
+        }
+        [Theory]
+        [ClassData(typeof(Parser_UNION_EXCEPT_Query_testdata))]
+        public void Parser_query_success(string str, QueryData expected)
+        {
+            //arrange
+            CompositionRoot compRoot = new CompositionRoot();
+            RecursiveDescentParser parser = compRoot.getParser();
+            parser.parse(str);
+            //act
+            QueryData actual = parser.query();
+            //assert
+            Assert.Equal(true, expected.GetType() == actual.GetType());
+            Assert.Equivalent(expected, actual);
+        }
 
 
 
