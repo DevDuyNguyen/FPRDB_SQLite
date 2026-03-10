05935b0da51a15ff84e675eecc5b77f9ac4f54eb
dqdq2608 <148328743+dqdq2608@users.noreply.github.com>
2026-03-10 16:26:28 +0700

    modify create new relation
    load schemas for combo box with method from database service

--- FPRDB_SQLite/GUI/frmNewRelation.cs
+++ FPRDB_SQLite/GUI/frmNewRelation.cs
@@ -1,4 +1,7 @@
-﻿using DevExpress.XtraEditors;
+﻿using BLL.Common;
+using BLL.DomainObject;
+using BLL.Services;
+using DevExpress.XtraEditors;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,9 +16,19 @@ namespace FPRDB_SQLite.GUI
 {
     public partial class frmNewRelation : DevExpress.XtraEditors.XtraForm
     {
-        public frmNewRelation()
+        private readonly CompositionRoot compRoot;
+        private readonly DatabaseService databaseService;
+        public frmNewRelation(CompositionRoot compRoot)
         {
             InitializeComponent();
+            this.compRoot = compRoot;
+            this.databaseService = compRoot.getDatabaseService();
+            loadSchemas();
+        }
+        private void loadSchemas()
+        {
+            List<FPRDBSchema> schemas = databaseService.getFPRDBSchemas();
+            cboSchemaName.Properties.Items.AddRange(schemas.Select(s => s.getSchemaName()).ToArray());
         }
     }
 }
