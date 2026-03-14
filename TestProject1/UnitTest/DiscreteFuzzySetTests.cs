a7c9783865dc1abeedf294a75e8af2eed4cd6329
DevDuyNguyen <kduy91809180@gmail.com>
2026-03-14 23:01:38 +0700

    DiscreteFuzzySet::isEqualTo()

--- TestProject1/UnitTest/DiscreteFuzzySetTests.cs
+++ TestProject1/UnitTest/DiscreteFuzzySetTests.cs
@@ -75,6 +75,39 @@ namespace TestProject1.UnitTest
             //assert
             Assert.Equal(expected, fs.getHeight(), 3);
         }
+        class isEqualTo_postive_negative_testdata:TheoryData<FuzzySet<float>, FuzzySet<float>, bool>
+        {
+            public isEqualTo_postive_negative_testdata()
+            {
+                Add(
+                new DiscreteFuzzySet<float>(new List<float> { 1.1f, 3.2f, 1.3f }, new List<float> { 0.2f, 0.2f, 0.3f, 1f }, "fs1", FieldType.distFS_FLOAT),
+                new DiscreteFuzzySet<float>(new List<float> { 1.1f, 1.2f, 4 }, new List<float> { 0.1f, 0.2f, 0.3f, 0.2f }, "fs2", FieldType.distFS_FLOAT),
+                false
+                );
+
+                Add(
+                new DiscreteFuzzySet<float>(new List<float> { 1.1f, 3.2f, 1.3f }, new List<float> { 0.2f, 0.2f, 0.3f, 1f }, "fs1", FieldType.distFS_FLOAT),
+                new DiscreteFuzzySet<float>(new List<float> { 1.1f, 3.2f, 1.3f }, new List<float> { 0.2f, 0.2f, 0.3f, 1f }, "fs1", FieldType.distFS_FLOAT),
+                true
+                );
+
+                Add(
+                new DiscreteFuzzySet<float>(new List<float> { 1.1f, 1.2f, 4 }, new List<float> { 0.1f, 0.2f, 0.3f, 0.2f }, "fs2", FieldType.distFS_FLOAT),
+                new DiscreteFuzzySet<float>(new List<float> { 1.1f, 1.2f, 4 }, new List<float> { 0.1f, 0.2f, 0.3f, 0.2f }, "fs2", FieldType.distFS_FLOAT),
+                true
+                );
+            }
+        }
+        [Theory]
+        [ClassData(typeof(isEqualTo_postive_negative_testdata))]
+        public void isEqualTo_sucess_fail(FuzzySet<float> fs1, FuzzySet<float> fs2, bool expected)
+        {
+            //arrange
+            //act
+            //assert
+            Assert.Equal(expected, fs1.isEqualTo(fs2));
+        }
+
 
     }
 }
